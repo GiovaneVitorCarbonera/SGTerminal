0cecdd7fdb91d9a832124d00602ea022b7c6a699
GiovaneVitorCarbonera <zehousnorton@gmail.com>
2026-06-03 11:44:27 -0300

    Fix Env Implementation

--- services/DBService.js
+++ services/DBService.js
@@ -1,20 +1,24 @@
-const { Client } = require('pg');
+require('dotenv').config();
 
-const connection = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'sua_senha',
-  database: 'api_carros',
-  port: 5432
+const { Pool } = require('pg');
+
+const connection = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
 });
 
-connection.connect((erro) => {
+connection.connect((erro, client, release) => {
   if (erro) {
     console.error('Erro ao conectar no banco:', erro);
     return;
   }
 
   console.log('Conectado ao PostgreSQL');
+
+  release();
 });
 
 module.exports = connection;
