3c15d8c717a45f664128ef4c195674ff78bd7f14
GiovaneVitorCarbonera <zehousnorton@gmail.com>
2026-06-03 11:25:38 -0300

    Add init Auth Route

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
-const carrosRoutes = require('./routes/carros');
+const authRoutes = require('./routes/auth.js');
+const accountsRoutes = require('./routes/accounts.js');
+const vehiclemovementsRoutes = require('./routes/vehiclemovements.js');
 
 app.use(express.json());
-app.use('/carros', carrosRoutes);
+app.use('/auth', authRoutes);
+app.use('/accounts', accountsRoutes);
+app.use('/vehicle-movements', vehiclemovementsRoutes);
 
 app.listen(8080, () => {
   console.log('Servidor executando na porta 8080');
